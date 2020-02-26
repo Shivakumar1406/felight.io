9da3ff611d5e29a69e5968034cb83233d49e9efc
Shivakumar1406 <shivukati14@gmail.com>
2020-02-26 09:27:32 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react';
+import Homepage from'./components/Homepage';
 
-
+import './App.css';
 
 class App extends Component{
-  constructor(){
-    super();
-    this.state={items:[],currentItem: {text:'',key:''}}
-  }
-  handleInput(e){
-    this.setState({currentItem:{text:e.target.value, key:Date.now()}}
-
-    )
-  }
-  addItem(e){
-e.preventDefault();
-  }
   render(){
     return(
-      <form id="to-do-form" onSumbit={this.addItem}>
-      <input type="text" placeholder="Enter text" value={this.state.currentItem.text} onChange={this.handleInput}></input>
-      <button type="submit">+</button>
-      </form>
+      <div>
+      <Homepage/>
+      </div>
     )
   }
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Homepage = () => (
+
+<div className='homepage'>
+    <div className='categories-container'> 
+       
+    
+        <div className='category-item'>
+       <div className='content-holder'> 
+          <h1 className='title'>Java Full Stack</h1>
+            <span className='subtitle'>Know more</span>
+        </div>
+    </div>    
+       
+    
+    <div className='category-item'>
+       <div className='content-holder'> 
+          <h1 className='title'>JavaScript Full Stack</h1>
+            <span className='subtitle'>Know more</span>
+        </div>
+    </div>  
+    
+    
+    
+    <div className='category-item'>
+       <div className='content-holder'> 
+          <h1 className='title'>Python Full Stack</h1>
+            <span className='subtitle'>Know more</span>
+        </div>
+    </div>
+    
+    
+    
+    <div className='category-item'>
+       <div className='content-holder'> 
+          <h1 className='title'>Data Science</h1>
+            <span className='subtitle'>Know more</span>
+        </div>
+    </div>    
+       
+</div>
+  </div>
+)
+
+       
+
+export default Homepage;
